a2aab4b4008a12797f8828ba2a94f921cda56a52
gzgzgzgzgzgz <zheng3@uci.edu>
2023-02-13 20:05:00 -0800

    feat: init springboot application

---
+++ src/test/java/com/coastal/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coastal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
