7e5b6e9095fbf9ff9509d7f5aa5e711011f3d9a0
gzgzgzgzgzgz <zheng3@uci.edu>
2023-02-23 11:52:35 -0800

    fix: exception body when request is invalid

---
+++ src/main/java/com0/coastalcasa/Utils/EntityExceptionHandler.java
@@ -0,0 +1,18 @@
+package com0.coastalcasa.Utils;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class EntityExceptionHandler {
+    @ExceptionHandler(value = { Exception.class })
+    public ResponseInfo<Object> handleAnyException(Exception ex, WebRequest request) {
+        return ResponseInfo.fail(ex.getMessage());
+    }
+}
