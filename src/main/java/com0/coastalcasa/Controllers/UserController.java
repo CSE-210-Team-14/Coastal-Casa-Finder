22bea720b5b2cab6b7f28dc4e2382aaf4028689c
jialexu <38922328+jialexu@users.noreply.github.com>
2023-02-23 20:28:51 -0800

    documentation #1

--- src/main/java/com0/coastalcasa/Controllers/UserController.java
+++ src/main/java/com0/coastalcasa/Controllers/UserController.java
@@ -4,12 +4,14 @@ import com0.coastalcasa.Mapper.LandlordMapper;
 import com0.coastalcasa.Models.Landlord;
 import com0.coastalcasa.Services.UserService;
 import com0.coastalcasa.Utils.ResponseInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-
+@Api(tags="UserController")
 @RestController
 @RequestMapping("/landlord")
 public class UserController {
@@ -23,12 +25,14 @@ public class UserController {
 
     private LandlordMapper landlordMapper;
 
+    @ApiOperation(value="Get all landlords", notes="Get a list of landlords")
     @GetMapping("/all")
     public ResponseInfo<List<Landlord>> getAll(){
         List<Landlord> all = landlordMapper.findAll();
         return ResponseInfo.success(all);
     }
 
+    @ApiOperation(value="Create a landlord user", notes="Create a landlord object: success if landlord has not been created before, else fail.")
     @PostMapping("/signup")
     private ResponseInfo<String> createLandLord(HttpServletResponse res, @RequestBody Landlord landlord){
         Boolean result = userService.signup(res, landlord);
@@ -39,6 +43,7 @@ public class UserController {
         }
     }
 
+    @ApiOperation(value="Landlord user login", notes="Check if the landlord user login information is correct.")
     @PostMapping("/signin")
     public ResponseInfo<String> login(HttpServletResponse response, @RequestBody Landlord landlord) {
         Boolean loginResult = userService.login(response, landlord);
